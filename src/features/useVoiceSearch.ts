2890410dadc921030e3bdb6ee94e41ee6e92e432
Fares <fares.melshafey@gmail.com>
2026-05-07 13:47:01 +0300

    solve conflect

--- src/features/useVoiceSearch.ts
+++ src/features/useVoiceSearch.ts
@@ -11,11 +11,6 @@ export function useVoiceSearch() {
   } = useMutation({
     mutationFn: (audioBlob: Blob) => sendVoiceSearch(audioBlob),
     onSuccess: () => {
-// <<<<<<< all-routes-api
-//       // You can add logic here to handle the successfully recognized text/routes
-//       // Or let the component handle it via the mutate callback
-// =======
-// >>>>>>> main
       toast.success("تم إرسال الصوت بنجاح");
     },
     onError: (err: any) => {
